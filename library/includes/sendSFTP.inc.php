1ee0c05d802a22be0851bc605b8ca6794ee55cc1
Ian Bettison <ian@bettison.me.uk>
2014-10-28 13:57:03 +0000

    final commit for the SMP2 project

--- library/includes/sendSFTP.inc.php
+++ library/includes/sendSFTP.inc.php
@@ -4,5 +4,4 @@ if(!defined("ROOT_FOLDER")){
     define('ROOT_FOLDER', $root);
 }
 require(ROOT_FOLDER."/SMP2/vendor/autoload.php");
-require_once("Net/SFTP.php");
 include(ROOT_FOLDER."/SMP2/library/Classes/sendSFTP.Class.php");
