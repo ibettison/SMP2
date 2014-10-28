479ef8ed96322f03f6313441776b2a658a9a0862
Ian Bettison <ian@bettison.me.uk>
2014-10-28 11:46:27 +0000

    Updated the package to include Composer and the PHPSECLIB library which should auto install if not present.

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit8214a77b71aea55363c7ce06930676ee::getLoader();
+return ComposerAutoloaderInit84b3511f175bf66aeddfcc8bfc7a611d::getLoader();
