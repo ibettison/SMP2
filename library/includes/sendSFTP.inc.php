73682abce5f18234a2a2e6cc9a50a009a312798e
Ian Bettison <ian@bettison.me.uk>
2014-10-28 11:47:03 +0000

    Updated the package to include Composer and the PHPSECLIB library which should auto install if not present.

--- library/includes/sendSFTP.inc.php
+++ library/includes/sendSFTP.inc.php
@@ -3,6 +3,6 @@ if(!defined("ROOT_FOLDER")){
     $root = $_SERVER["DOCUMENT_ROOT"];
     define('ROOT_FOLDER', $root);
 }
-set_include_path(get_include_path(). PATH_SEPARATOR . ROOT_FOLDER."/SMP2/vendor/phpseclib/phpseclib/phpseclib/");
+require(ROOT_FOLDER."/SMP2/vendor/autoload.php");
 require_once("Net/SFTP.php");
 include(ROOT_FOLDER."/SMP2/library/Classes/sendSFTP.Class.php");
