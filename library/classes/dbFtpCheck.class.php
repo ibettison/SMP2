f97b7a337c2478d34ef8aa79f56fd6fef9e2f287
Ian Bettison <ian@bettison.me.uk>
2014-11-05 17:29:05 +0000

    testing

--- library/classes/dbFtpCheck.class.php
+++ library/classes/dbFtpCheck.class.php
@@ -4,5 +4,6 @@ if(!defined("ROOT_FOLDER")){
 	define('ROOT_FOLDER', $root);
 }
 require(ROOT_FOLDER."SMP2/vendor/autoload.php");
-include("library/classes/sendSFTP.Class.php");
-sftpConnect::createSFTPConnection($_POST["ftpServer"], $_POST["ftpUserName"], $_POST["ftpPassword"], $_POST["ftpSendFolder"], $_POST["ftpResultFolder"],$_POST["ftpArchiveFolder"]);
+include(ROOT_FOLDER."SMP2/library/classes/sendSFTP.Class.php");
+//sftpConnect::createSFTPConnection($_POST["ftpServer"], $_POST["ftpUserName"], $_POST["ftpPassword"], $_POST["ftpSendFolder"], $_POST["ftpResultFolder"],$_POST["ftpArchiveFolder"]);
+echo "returned";
