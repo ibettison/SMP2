c647b00e38d6025b3b69651b1f5ee01f14894c98
Ian Bettison <ian@bettison.me.uk>
2014-11-06 12:15:41 +0000

    Shell Script added for the setup of directories and the install of composer libraries

--- setup.sh
+++ setup.sh
@@ -15,6 +15,9 @@ cd ..
 php composer.phar install
 cd library
 chmod 777 includes
-echo "The permissions have been changed to allow you to save additional settings files within the SMP2 application./r/nOnce all connections have been tested the security.sh script needs to be ran./r/nThis will secure the install of SMP2 and will prevent hackers from accessing your Web service."
+printf "**************IMPORTANT*****************"
+printf "*** Please read the message below    ***"
+printf "****************************************"
+printf "The permissions have been changed to allow you to save additional settings files within the SMP2 application.\n\rOnce all connections have been tested the security.sh script needs to be ran.\n\rThis will secure the install of SMP2 and will prevent hackers from accessing your Web service."
  
 
