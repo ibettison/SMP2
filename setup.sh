00f5c6d5b7faf60a3817d34feb75099873cbc8b7
Ian Bettison <ian@bettison.me.uk>
2014-11-10 11:02:14 +0000

    Changed the process from testing to production - setting defined file paths

--- setup.sh
+++ setup.sh
@@ -1,22 +1,11 @@
 #!/bin/sh
 # setup script for the SMP2 project
 # create and setup the permissions on the xml file locations
-chmod 766 xml-documents
 cd xml-documents
 mkdir files-sent
-chmod 766 files-sent
 mkdir files-received
-chmod 766 files-received
 mkdir files-archived
-chmod 766 files-archived
-chmod 766 schema
 cd ..
 # now install the third party libraries required by the SMP2 project 
 # using Composer
 php composer.phar install
-cd library
-chmod 777 includes
-printf "**************IMPORTANT*****************\n"
-printf "*** Please read the message below    ***\n"
-printf "****************************************\n"
-printf "The permissions have been changed to allow you to save additional\nsettings files within the SMP2 application.\nOnce all connections have been tested the security.sh script needs to be ran.\nThis will secure the install of SMP2 and will\nprevent hackers from accessing your Web service."
