b39ebb6a46bec0c8daa17d3a60f0f1a49fe50b6f
Ian Bettison <ian@bettison.me.uk>
2014-11-19 14:00:37 +0000

    Security scripts updated

--- setup.sh
+++ setup.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 # setup script for the SMP2 project
-# create and setup the permissions on the xml file locations
-cd xml-documents
-mkdir files-sent
-mkdir files-received
-mkdir files-archived
-cd ..
-# now install the third party libraries required by the SMP2 project 
+
+# lets install the third party libraries required by the SMP2 project
 # using Composer
 php composer.phar install
+chmod 777 ./library/includes
+echo "***********************************************"
+echo "*** the folder to save database and ftp     ***"
+echo "*** has been opened so that the settings    ***"
+echo "*** can be saved after this setup has ran.  ***"
+echo "*** You MUST run the security script to     ***"
+echo "*** secure your website from HACKERS...     ***"
+echo "*** Make sure you completely follow the     ***"
+echo "*** remaining instructions.                 ***"
+echo "***********************************************"
